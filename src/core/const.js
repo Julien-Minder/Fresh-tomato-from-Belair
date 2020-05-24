b6b8249c76c90ae0d12b8aae9e8fbadeac9a1ff3
Julien <julien.minder@gmail.com>
2020-05-24 17:15:21 +0200

    ready to upload online

--- src/core/const.js
+++ src/core/const.js
@@ -1,3 +1,3 @@
-export const SESSION_DURATION = 3;
-export const PAUSE_DURATION = 3;
+export const SESSION_DURATION = 1500; //25min
+export const PAUSE_DURATION = 300; //5min
 export const NBSP = "\u00a0";
